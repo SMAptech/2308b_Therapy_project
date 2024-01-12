12430ae2fa8157996980ad680342fde4afa85687
smaptech <sadam@aptechgdn.net>
2024-01-12 11:35:50 +0500

    Therapy project

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert("Hello World")
+
+$('gallery-background').onclick(function(){
+    open()
+})
+
+// $(document).ready(function(){
+//     $("#btn1").mouseover(function(){
+//       $("#box").animate({width: "300px"});
+//     });
+//     $("#btn2").mouseover(function(){
+//       $("#box").animate({width: "100px"});
+//     });
+//   });
+  
